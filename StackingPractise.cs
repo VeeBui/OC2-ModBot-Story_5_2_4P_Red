cd249710a9adc3adffe0c7cf3ee8ef140ca6194c
VeeBui <veeeebui@gmail.com>
2025-10-24 16:10:07 +1100

    Initial commit of project files

---
+++ StackingPractise.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+using Story52_Stacking.Utils;
+using Team17.Online.Multiplayer.Messaging;
+using HarmonyLib;
+
+namespace Story52_Stacking
+{
+    [BepInPlugin("MyGUID", "Story 5-2 Stacking", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        // Bring in functions
+        private Actions actions = new Actions();
+        private LevelInfo levelInfo = new LevelInfo();
+        // Constants for comparison
+        Vector3 offset = new Vector3(0.0f, 0.5f, 0.0f); // items are usually 0.5 above counters
+        const float tol = 0.01f;
+
+        // global variables
+        bool setLettuce = false;
+        int meatCount = 0;
+        int phase = 0;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Debug.Log($"Plugin {"MyGUID"} is loaded!");
+            Debug.Log("Hello world from Stacking Practise Mod/Bot (Story 5-2)");
+
+        }
+        private void Update()
+        {
+            // always check if we are in the level
+            var controller = FindObjectOfType<ServerKitchenFlowControllerBase>();
+            try
+            {
+                levelInfo.CheckLevelState(controller);
+                if (levelInfo.inLevel)
+                {
+                    levelInfo.UpdateLevelData(controller);
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError($"Error finding controller/level config: {ex}");
+            }
+
+            // Now with level info being tracked properly, start doing stuff
+            if (levelInfo.name == "Balloon_5_2_2P" && levelInfo.timer > 0.1f)
+            {
+                Logger.LogInfo("In level");
+
+                switch (phase)
+                {
+                    case 0:
+                        // Phase 0 = starting sequence
+                        break;
+                    case 1:
+                        // Phase 1 = normal run, stack and bun
+                        break;
+                    case 2:
+                        // Phase 2 = ending sequence
+                        break;
+                    default:
+                        break;
+                }
+            }
+            
+        }
+    }
+}
---
+++ Utils/LevelInfo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Story52_Stacking.Utils
+{
+    public class LevelInfo
+    {
+        public string name = "No Level";
+        public float timer = -1.0f;
+        //public int levelID = -1;
+        //public int DLC = -42;
+
+        public bool inLevel = false;
+        public void CheckLevelState(ServerKitchenFlowControllerBase controller)
+        {
+            bool wasInLevel = inLevel;
+            inLevel = (controller != null);
+            // check if state changed
+            if (inLevel && !wasInLevel)
+            {
+                // if level state changed to be in level
+                GetLevelData();
+            }
+            else if (!inLevel && wasInLevel)
+            {
+                // if level state changed to have exited level
+                name = "No Level";
+                timer = -1.0f;
+                //levelID = -1;
+                //DLC = -42;
+            }
+
+            //
+        }
+        public void GetLevelData()
+        {
+            // get the level name
+            var levelConfig = GameUtils.GetLevelConfig();
+            if (levelConfig != null)
+            {
+                name = levelConfig.name;
+            }
+            // some other stuff
+            /*
+            GameSession gameSession = GameUtils.GetGameSession();
+            if (gameSession != null)
+            {
+                DLC = gameSession.DLC;
+            }
+            levelID = GameUtils.GetLevelID();
+            */
+        }
+        public void UpdateLevelData(ServerKitchenFlowControllerBase controller)
+        {
+            if (controller != null)
+            {
+                timer = controller?.RoundTimer?.TimeElapsed ?? -1.0f;
+            }
+        }
+
+    }
+}
